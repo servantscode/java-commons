f5996a98cc1d0aba0e098af8307f96fd246c335e
Greg Leitheiser <greg@servantscode.org>
2018-11-06 12:25:30 -0600

    Reusable stuff from person-svc

---
+++ src/main/java/org/servantscode/commons/StringUtils.java
@@ -0,0 +1,7 @@
+package org.servantscode.commons;
+
+public class StringUtils {
+    public static boolean isEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+}
---
+++ src/test/java/org/servantscode/commons/testAutoCompleteComparator.java
@@ -0,0 +1,29 @@
+package org.servantscode.commons;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class testAutoCompleteComparator {
+
+    @Test
+    public void testSort() {
+        String search = "An";
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Stephanie Leitheiser");
+        names.add("Joshua Anberline");
+        names.add("Anneliese Leitheiser");
+        names.add("Joshua\tAnberline");
+
+        names.sort(new AutoCompleteComparator(search));
+
+        ArrayList<String> expectedNames = new ArrayList<>(4);
+        expectedNames.add("Anneliese Leitheiser");
+        expectedNames.add("Joshua Anberline");
+        expectedNames.add("Joshua\tAnberline");
+        expectedNames.add("Stephanie Leitheiser");
+
+        Assert.assertArrayEquals("names not sorted correctly", expectedNames.toArray(), names.toArray());
+    }
+}
