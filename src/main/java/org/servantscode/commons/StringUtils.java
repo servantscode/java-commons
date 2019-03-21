71d6ae299631b974e17cf458430effb097246e27
Greg Leitheiser <greg@servantscode.org>
2019-03-21 16:48:38 -0500

    Added StringUtils.isSet

--- src/main/java/org/servantscode/commons/StringUtils.java
+++ src/main/java/org/servantscode/commons/StringUtils.java
@@ -4,4 +4,8 @@ public class StringUtils {
     public static boolean isEmpty(String s) {
         return s == null || s.isEmpty();
     }
+
+    public static boolean isSet(String s) {
+        return s != null && !s.isEmpty();
+    }
 }
