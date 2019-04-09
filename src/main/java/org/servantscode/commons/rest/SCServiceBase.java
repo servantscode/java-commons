02c91ec4451fda2841e4ba0dfdc6da112219c055
Greg Leitheiser <greg@servantscode.org>
2019-04-09 17:12:05 -0500

    Added getUserId to service base

--- src/main/java/org/servantscode/commons/rest/SCServiceBase.java
+++ src/main/java/org/servantscode/commons/rest/SCServiceBase.java
@@ -1,9 +1,11 @@
 package org.servantscode.commons.rest;
 
 import org.servantscode.commons.security.PermissionManager;
+import org.servantscode.commons.security.SCPrincipal;
 
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.core.SecurityContext;
 
 public class SCServiceBase {
     protected void verifyUserAccess(String permission) {
@@ -12,6 +14,10 @@ public class SCServiceBase {
 
         if(!PermissionManager.canUser(permission))
             throw new ForbiddenException("Requested action is not available");
+    }
+
+    protected int getUserId(SecurityContext context) {
+        return ((SCPrincipal)context.getUserPrincipal()).getUserId();
     }
 
     protected boolean userHasAccess(String permission) {
