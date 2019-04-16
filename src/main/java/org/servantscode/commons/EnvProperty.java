5f2b4892a2ac1dc15c720763ff9ae275beb21c14
Greg Leitheiser <greg@servantscode.org>
2019-04-16 15:27:23 -0500

    clean up logging to not write env properties to logs

--- src/main/java/org/servantscode/commons/EnvProperty.java
+++ src/main/java/org/servantscode/commons/EnvProperty.java
@@ -3,15 +3,20 @@ package org.servantscode.commons;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import static org.servantscode.commons.StringUtils.isEmpty;
+import static org.servantscode.commons.StringUtils.isSet;
 
 public class EnvProperty {
     private static final Logger LOG = LogManager.getLogger(EnvProperty.class);
 
     public static String get(String propName, String defaultValue) {
         String lookup = get(propName);
-        LOG.debug(String.format("Got env property %s value: %s", propName, lookup));
-        return !isEmpty(lookup)? lookup: defaultValue;
+        if(isSet(lookup)) {
+            LOG.debug(String.format("Successfully retrieved env property %s", propName));
+            return lookup;
+        } else {
+            LOG.warn(String.format("Could not retrieve env property %s. Using default value.", propName));
+            return defaultValue;
+        }
     }
 
     public static String get(String propName) {
