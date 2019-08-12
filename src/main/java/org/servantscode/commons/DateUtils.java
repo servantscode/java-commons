7259c078b020eb9bc0417a21b9557b0ba034f159
Greg Leitheiser <greg@servantscode.org>
2019-08-12 10:43:08 -0500

    Event export

--- src/main/java/org/servantscode/commons/DateUtils.java
+++ src/main/java/org/servantscode/commons/DateUtils.java
@@ -2,15 +2,42 @@ package org.servantscode.commons;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.servantscode.commons.db.ConfigDB;
 
 import java.time.*;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+import static org.servantscode.commons.StringUtils.isEmpty;
 
 public class DateUtils {
     private static final Logger LOG = LogManager.getLogger(DateUtils.class);
+
+    private static ConfigDB db = new ConfigDB();
+
+    private static final ZoneId DEFAULT_TIMEZONE = ZoneId.of("America/Chicago");
+
+    public static ZoneId getTimeZone() {
+        LOG.trace("Getting timezone");
+        String zone;
+        try {
+            zone = db.getConfiguration("timezone");
+        } catch (Throwable t) {
+            LOG.error("Failed to get timezone configuration! ", t);
+            throw t;
+        }
+        LOG.trace("got configuration: " + zone);
+        if(isEmpty(zone)) return DEFAULT_TIMEZONE;
+
+        try {
+            LOG.trace("getting zoneId");
+            return ZoneId.of(zone);
+        } catch(DateTimeException e) {
+            LOG.error("Invalid timezone string encountered: " + zone + ". Using US Central timezone as default.");
+            return DEFAULT_TIMEZONE;
+        }
+    }
 
     public static ZonedDateTime parse(String input) {
         return parse(input, (ZonedDateTime) null);
--- src/main/java/org/servantscode/commons/db/ReportStreamingOutput.java
+++ src/main/java/org/servantscode/commons/db/ReportStreamingOutput.java
@@ -1,65 +1,122 @@
 package org.servantscode.commons.db;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.servantscode.commons.DateUtils;
+
 import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.DateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.servantscode.commons.db.DBAccess.convert;
 
 public abstract class ReportStreamingOutput implements StreamingOutput {
-    private List<String> fields;
+    private static Logger LOG = LogManager.getLogger(ReportStreamingOutput.class);
 
+    public static final String CRLF = "\r\n";
+    private List<String> fields;
+    ResultSetMetaData metaData;
+
+    private ZoneId timezone;
+
+    // StreamingOutput performs its function outside of the context of the web service call.
+    // The constructor MUST ensure that any context related data is cached for operation.
     public ReportStreamingOutput(List<String> fields) {
         this.fields = fields;
+
+        //Get timezone requires OrganizationContext to be populated.
+        this.timezone = DateUtils.getTimeZone();
+    }
+
+    // ----- Protected -----
+    protected void writeCsv(OutputStream output, ResultSet rs) throws IOException {
+        try {
+            metaData = rs.getMetaData();
+            String data = null;
+
+            writeLine(output, prepareHeaders());
+            while((data = prepareData(rs)) != null)
+                writeLine(output, data);
+        } catch (SQLException e) {
+            throw new IOException("Could not read report from results", e);
+        }
     }
 
     // ----- Private -----
-    protected void writeCsv(OutputStream output, ResultSet rs) throws IOException {
-        output.write(prepareHeaders().getBytes());
-        String data = null;
-        while((data = prepareData(rs)) != null)
-            output.write(data.getBytes());
+    private void writeLine(OutputStream stream, String line) throws IOException {
+        stream.write(line.getBytes());
+        stream.write(CRLF.getBytes());
     }
 
-    protected String prepareHeaders() {
-        String preparedData = "";
-
-        boolean first = true;
-        for(String field: fields) {
-            if(first) {
-                first = false;
-            } else {
-                preparedData += ",";
-            }
-
-            preparedData += field;
-        }
-
-        return preparedData + "\r\n";
+    private String prepareHeaders() {
+        return String.join(",", fields);
     }
 
-    protected String prepareData(ResultSet rs) throws IOException {
-        String preparedData = "";
+    private String prepareData(ResultSet rs) throws SQLException {
+        if(!rs.next())
+            return null;
 
-        try {
-            if(!rs.next())
-                return null;
+        LinkedList<String> data = new LinkedList<>();
+        for(String field: fields)
+            data.add(extractColumn(rs, field));
 
-            boolean first = true;
-            for(String field: fields) {
-                if(first) {
-                    first = false;
-                } else {
-                    preparedData += ",";
-                }
+        return data.stream().map(d -> d == null? "": d).collect(Collectors.joining(","));
+    }
 
-                preparedData += rs.getString(field);
-            }
+    private String extractColumn(ResultSet rs, String field) throws SQLException {
+        int columnType = metaData.getColumnType(rs.findColumn(field));
+        if(columnType != Types.VARCHAR &&
+           columnType != Types.BIT &&
+           columnType != Types.INTEGER &&
+           columnType != Types.DATE &&
+           columnType != Types.TIMESTAMP &&
+           columnType != Types.TIMESTAMP_WITH_TIMEZONE &&
+           columnType != Types.ARRAY)
+            LOG.debug(String.format("Processing unexpected field %s of type: %d. Will try to report it as string data.", field, columnType));
 
-        } catch (SQLException e) {
-            throw new IOException("Could not read field from results", e);
+        switch (columnType) {
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+            case Types.TIMESTAMP:
+                return extractZonedDateTime(rs, field);
+            case Types.ARRAY:
+                return extractArrayData(rs, field);
+            default:
+                return rs.getString(field);
         }
-        return preparedData + "\r\n";
+    }
+
+    private String extractArrayData(ResultSet rs, String field) throws SQLException {
+        Array arr = rs.getArray(field);
+        if(arr == null)
+            return null;
+
+        if(arr.getBaseType() != Types.VARCHAR && arr.getBaseType() != Types.INTEGER)
+            LOG.debug(String.format("Extracting array data from %s with type: %s. Data: %s", field, arr.getBaseTypeName(), arr.toString()));
+
+        List<String> arrData = new LinkedList<>();
+        ResultSet arrs = arr.getResultSet();
+        while(arrs.next())
+            arrData.add(arrs.getString(2)); //data is in second column
+
+        if(arrData.isEmpty())
+            return null;
+
+        return String.join("|", arrData);
+    }
+
+    private String extractZonedDateTime(ResultSet rs, String field) throws SQLException {
+        ZonedDateTime zdt = convert(rs.getTimestamp(field));
+        if(zdt == null)
+            return null;
+
+        return zdt.withZoneSameInstant(timezone).format(DateTimeFormatter.ofPattern("YYYY-mm-dd hh:mm a"));
     }
 }
