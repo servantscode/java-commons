78e1e0a9c31538d0818b10beb6c5e60dc5874b8c
Greg Leitheiser <greg@servantscode.org>
2019-12-20 11:29:11 -0600

    Donation report by year

--- src/main/java/org/servantscode/commons/security/OrganizationContext.java
+++ src/main/java/org/servantscode/commons/security/OrganizationContext.java
@@ -1,17 +1,27 @@
 package org.servantscode.commons.security;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.servantscode.commons.Organization;
 import org.servantscode.commons.db.OrganizationDB;
 
 public class OrganizationContext {
+    private static final Logger LOG = LogManager.getLogger(OrganizationContext.class);
+
     private static ThreadLocal<OrganizationContext> LOCAL_INSTANCE = new ThreadLocal<>();
     private static OrganizationDB db = new OrganizationDB();
 
     public static void enableOrganization(String hostName) { LOCAL_INSTANCE.set(new OrganizationContext(hostName)); }
 
-    public static Organization getOrganization() { return LOCAL_INSTANCE.get().getEnabledOrganization(); }
+    public static Organization getOrganization() {
+        OrganizationContext context = LOCAL_INSTANCE.get();
+        return context == null? null: context.getEnabledOrganization();
+    }
 
-    public static int orgId() { return LOCAL_INSTANCE.get().getEnabledOrganization().getId(); }
+    public static int orgId() {
+        OrganizationContext context = LOCAL_INSTANCE.get();
+        return context == null? 0: context.getEnabledOrganization().getId();
+    }
 
     public static void clearEnabledOrganization() { LOCAL_INSTANCE.remove(); }
 
@@ -19,7 +29,7 @@ public class OrganizationContext {
     private Organization enabledOrganization;
 
     private OrganizationContext(String hostName) {
-        System.out.println("Organization set to: " + hostName);
+        LOG.debug("Organization set to: " + hostName);
         enabledOrganization = db.getOrganization(hostName);
     }
 
