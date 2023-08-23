1d9ea203f7d743c631f70b680a9e264040fab858
nick <nicholas.w.paul@gmail.com>
2023-08-22 21:34:08 -0500

    Add fields, select, from, join, and where into the insert builder.
    This also has the functionality to not contain the values() statement when needed.

--- src/main/java/org/servantscode/commons/search/InsertBuilder.java
+++ src/main/java/org/servantscode/commons/search/InsertBuilder.java
@@ -4,20 +4,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.servantscode.commons.security.OrganizationContext;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
 import static org.servantscode.commons.StringUtils.isSet;
 
 public class InsertBuilder extends SqlBuilder {
     private static Logger LOG = LogManager.getLogger(QueryBuilder.class);
 
-    private enum BuilderState {START, INTO, VALUES, DONE };
+    private enum BuilderState {START, INTO, FIELDS, SELECT, FROM, JOIN, WHERE, VALUES, ON, DONE };
 
     private String table = null;
+    private String select = null;
+    private String from = null;
     private List<String> fields = new LinkedList<>();
+    private List<String> joins = new LinkedList<>();
+    protected List<String> wheres = new LinkedList<>();
+    protected String on = null;
+
+    private boolean addValueStatment = false;
 
     private BuilderState state = BuilderState.START;
 
@@ -29,12 +36,46 @@ public class InsertBuilder extends SqlBuilder {
         return this;
     }
 
+    public InsertBuilder field (String... fields){
+        setState(BuilderState.FIELDS);
+        this.fields.addAll(List.of(fields));
+        return this;
+    }
+
+    public InsertBuilder field(String field) {
+        setState(BuilderState.FIELDS);
+        this.fields.add(field);
+        return this;
+    }
+
     public InsertBuilder value(String field, Object value) {
         setState(BuilderState.VALUES);
         this.fields.add(field);
         this.values.add(value);
+        addValueStatment = true;
         return this;
     }
+
+    public InsertBuilder select(String select) {
+        setState(BuilderState.SELECT);
+        this.select = select;
+        return this;
+    }
+
+    public InsertBuilder from(String from){
+        setState(BuilderState.FROM);
+        this.from = from;
+        return this;
+    }
+
+    public InsertBuilder leftJoin(String join, Object... values) {
+        setState(BuilderState.JOIN);
+        this.joins.add("LEFT JOIN " + join);
+        this.values.addAll(Arrays.asList(values));
+        return this;
+    }
+
+
 
     public InsertBuilder inOrg() {
         setState(BuilderState.VALUES);
@@ -50,13 +91,51 @@ public class InsertBuilder extends SqlBuilder {
         return this;
     }
 
+    public InsertBuilder where(String clause, Object value) {
+        this.wheres.add(clause);
+        values.add(value);
+        return this;
+    }
+
+    public InsertBuilder where(String clause, Object... value) {
+        this.wheres.add(clause);
+        values.addAll(Arrays.asList(value));
+        return this;
+    }
+
+    public InsertBuilder where(String clause) {
+        this.wheres.add(clause);
+        return this;
+    }
+
+    public InsertBuilder on(String on){
+       setState(BuilderState.ON);
+       this.on= on;
+       return this;
+    }
+
+
     @Override
     public String getSql() {
         setState(BuilderState.DONE);
         StringBuilder sql = new StringBuilder();
         sql.append("INSERT INTO ").append(table);
         sql.append(" (").append(String.join(", ", fields)).append(") ");
-        sql.append(" VALUES (").append(fields.stream().map(f -> "?").collect(Collectors.joining(", "))).append(")");
+        if(isSet(select))
+            sql.append(" SELECT ").append(select);
+        if(isSet(from))
+            sql.append(" FROM ").append(from);
+        if(!joins.isEmpty())
+            sql.append(" ").append(String.join(" ", joins));
+        if(!wheres.isEmpty())
+            sql.append(" WHERE ").append(String.join(" AND ", wheres));
+        if(addValueStatment)
+            sql.append(" VALUES (").append(fields.stream().map(f -> "?").collect(Collectors.joining(", "))).append(")");
+
+        if(isSet(on))
+            sql.append(" ON ").append(on);
+
+
         return sql.toString();
     }
 
